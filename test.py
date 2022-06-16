9f36fe1c41520d3d144e87d3cfbdcbec9141870e
Adrian Robert Doroteo <adrian56th@gmail.com>
2022-06-16 19:24:13 +0800

    fixed test file

--- test.py
+++ test.py
@@ -13,12 +13,12 @@ ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 ser.reset_input_buffer()
 x = True
 while x == True:
-
     now = datetime.now()
     date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 
-    line = ser.readline().decode('utf-8').rstrip()
-    sensorData = line.split(",")
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        sensorData = line.split(",")
 
    # temp = sensorData[0]
    # co = sensorData[1]
